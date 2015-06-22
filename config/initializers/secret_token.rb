5a9ed47e8c4fc8bcc400bf36c24bfb53f8b54059
Ilya Zvyagin <hunter@lustin.org>
2015-06-22 21:50:06 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '6fa55e80f09aeda06ca6f7fdd4039ff2e8517fc25670c4e8d64fae326464952af86b65365833621fb976ae9d35148b98d578a2b41170c2a6d8985368ab403078'
